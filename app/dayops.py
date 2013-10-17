f7aabfa802eb8a150533165cdd99c0f822673f3b
ozanyildiz <ozanyyildiz@gmail.com>
2013-10-17 23:30:39 +0300

    Previous and next day links done, hopefully

--- app/dayops.py
+++ app/dayops.py
@@ -3,13 +3,16 @@ from datetime import datetime, timedelta
 def get_today():
 	return datetime.utcnow()
 
-def get_tomorrow(date):
+def get_next_day(date):
 	one_day = one_day_diff()
-	return get_formatted_date(date + one_day)
+	return date + one_day
 	
-def get_yesterday(date):
+def get_prev_day(date):
 	one_day = one_day_diff()
-	return get_formatted_date(date - one_day)
+	return date - one_day
+
+def construct_datetime_obj(year, month, day):
+	return datetime(year, month, day)
 
 def get_formatted_date(date):
 	return to_string(date.year, date.month, date.day)
--- app/views.py
+++ app/views.py
@@ -1,21 +1,20 @@
 from flask import render_template
 import mongodb
-import dayops
+from dayops import *
 from app import app
 
 @app.route('/')
 def index():
-	today = dayops.get_today()
-	yesterday = dayops.get_yesterday(today)
-	tomorrow = dayops.get_tomorrow(today)
-	stories = mongodb.get_all_stories()
+	today = get_today()
+	stories = mongodb.get_stories(get_formatted_date(today))
 	return render_template("index.html",
 		page_title="Top stories of Yesterday", stories=stories,
-		prev_day=yesterday, next_day=tomorrow)
+		prev_day=get_prev_day(today), next_day=get_next_day(today))
 
 @app.route('/<int:year>-<int:month>-<int:day>')
 def archive(year, month, day):
-	date = dayops.to_string(year, month, day)
-	stories = mongodb.get_stories(date)
-	return render_template("index.html", 
-		page_title="Top stories of 17 Oct, 2013", stories=stories)
+	date = construct_datetime_obj(year, month, day)
+	stories = mongodb.get_stories(get_formatted_date(date))
+	return render_template("index.html",
+		page_title="Top stories of 17 Oct, 2013", stories=stories,
+		prev_day=get_prev_day(date), next_day=get_next_day(date))
