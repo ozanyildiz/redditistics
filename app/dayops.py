c161769be24d0b0060bdd65cc9b68f1d66850760
ozanyildiz <ozanyyildiz@gmail.com>
2013-11-02 11:55:10 +0200

    Convert all tabs to spaces

--- app/dayops.py
+++ app/dayops.py
@@ -1,36 +1,36 @@
 from datetime import datetime, timedelta
 
 def get_today():
-	return datetime.utcnow()
+    return datetime.utcnow()
 
 def get_next_day(date):
-	one_day = one_day_diff()
-	return date + one_day
-	
+    one_day = one_day_diff()
+    return date + one_day
+
 def get_prettified(date):
-	if is_today(date):
-		return "Today"
-	return date.strftime('%B %d, %Y')
+    if is_today(date):
+        return "Today"
+    return date.strftime('%B %d, %Y')
 
 def is_today(date):
-	today = get_today()
-	if date.day == today.day and date.month == today.month and date.year == today.year:
-		return True
-	else:
-		return False
-		
+    today = get_today()
+    if date.day == today.day and date.month == today.month and date.year == today.year:
+        return True
+    else:
+        return False
+
 def get_prev_day(date):
-	one_day = one_day_diff()
-	return date - one_day
+    one_day = one_day_diff()
+    return date - one_day
 
 def construct_datetime_obj(year, month, day):
-	return datetime(year, month, day)
+    return datetime(year, month, day)
 
 def get_formatted_date(date):
-	return to_string(date.year, date.month, date.day)
+    return to_string(date.year, date.month, date.day)
 
 def one_day_diff():
-	return timedelta(days=1)
+    return timedelta(days=1)
 
 def to_string(year, month, day):
-	return str(year) + '-' + str(month) + '-' + str(day).zfill(2)
+    return str(year) + '-' + str(month) + '-' + str(day).zfill(2)
--- app/db_loader.py
+++ app/db_loader.py
@@ -6,30 +6,30 @@ import pymongo
 AN_HOUR = 3600 # in seconds
 
 def get_db_stories():
-	connection = pymongo.Connection("mongodb://localhost", safe=True)
-	db = connection.reddit
-	return db.stories
+    connection = pymongo.Connection("mongodb://localhost", safe=True)
+    db = connection.reddit
+    return db.stories
 
 def get_front_page_stories():
-	reddit_page = urllib2.urlopen("http://www.reddit.com/.json")
-	parsed = json.load(reddit_page)
-	return parsed['data']['children']
-	
+    reddit_page = urllib2.urlopen("http://www.reddit.com/.json")
+    parsed = json.load(reddit_page)
+    return parsed['data']['children']
+
 def get_pruned_story(story_data):
-	s = dict()
-	s['title'] = story_data['title']
-	s['url'] = story_data['url']
-	s['subreddit'] = story_data['subreddit']
-	s['thumbnail'] = story_data['thumbnail']
-	s['domain'] = story_data['domain']
-	s['score'] = story_data['score']
-	s['permalink'] = story_data['permalink']
-	s['num_comments'] = story_data['num_comments']
-	s['created_epoch'] = story_data['created']
-	s['created'] = time.strftime('%Y-%m-%d', time.gmtime(story_data['created']))
-	
-	return s
-	
+    s = dict()
+    s['title'] = story_data['title']
+    s['url'] = story_data['url']
+    s['subreddit'] = story_data['subreddit']
+    s['thumbnail'] = story_data['thumbnail']
+    s['domain'] = story_data['domain']
+    s['score'] = story_data['score']
+    s['permalink'] = story_data['permalink']
+    s['num_comments'] = story_data['num_comments']
+    s['created_epoch'] = story_data['created']
+    s['created'] = time.strftime('%Y-%m-%d', time.gmtime(story_data['created']))
+
+    return s
+
 def main():
     stories = get_db_stories()
     #while True:
@@ -39,7 +39,7 @@ def main():
         #print pruned_story
         stories.update({'permalink': pruned_story['permalink']}, pruned_story, True)
 #time.sleep(AN_HOUR)
-	
+
 
 if __name__ == "__main__":
-	main()
+    main()
