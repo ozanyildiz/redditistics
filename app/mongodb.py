a6a44f1f10ac0f0f5c1d0e1cd0e76d218e7ad4ea
ozanyildiz <ozanyyildiz@gmail.com>
2013-11-24 15:57:26 +0200

    Popular subreddits shown on piechart.

--- app/mongodb.py
+++ app/mongodb.py
@@ -8,6 +8,12 @@ def get_all_stories():
 def get_stories(date):
     return mongo.db.stories.find({'created': date}).sort("score", -1)
 
+def get_popular_subreddits(date):
+    subreddits =  mongo.db.stories.aggregate([{'$match': {'created': date}}, {'$group': {'_id': {'subreddit': '$subreddit'},
+                                               'number': {'$sum':1}}}, {'$sort': {'number': -1}}, {'$limit': 10}])
+    return subreddits['result']
+
+
 def json_encoder(cursor):
     json_docs = []
     for doc in cursor:
--- app/views.py
+++ app/views.py
@@ -10,9 +10,10 @@ def archive(year=get_today().year, month=get_today().month, day=get_today().day)
     print date
     print get_formatted_date(date)
     stories = mongodb.get_stories(get_formatted_date(date))
+    popular_subreddits = mongodb.get_popular_subreddits(get_formatted_date(date))
     page_title = "Top stories of " + get_prettified(date)
     return render_template("index.html",
-        page_title=page_title, stories=stories,
+        page_title=page_title, stories=stories, popular_subreddits=popular_subreddits,
         prev_day=get_prev_day(date), next_day=get_next_day(date),
         is_today=is_today(date))
 
